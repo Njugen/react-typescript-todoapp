34d03261dc4e7042b7ffb61f0b5a46c2c76222d7
Njugen <privat_thai_nguyen@hotmail.com>
2020-07-26 17:11:21 +0300

    Added even more logic to the calendar. Like calendar changing accordingly to what the user selects from the selectors or by clicking back/forward arrows

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./../css/Calendar.css";
-interface StateRules {}
+import IconComponent from "./Icon";
+
+interface StateRules {
+  selectedMonth: number;
+  selectedYear: number;
+}
 
 interface PropRules {
   id: string;
@@ -12,17 +17,32 @@ type DateRangeInfo = {
   padding: number;
 };
 
-class CalendarComponent extends Component<PropRules, StateRules> {
-  // Get an array of all years from startYear uptil and including endYear
-  private weekdays: string[] = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+type MonthInfo = {
+  label: string;
+  number: number;
+};
 
+type TodayInfo = {
+  date: number;
+  month: number;
+  year: number;
+};
+
+class CalendarComponent extends Component<PropRules, StateRules> {
+  state = {
+    selectedMonth: 3,
+    selectedYear: 2021,
+  };
+
+  // Get an array of all years from startYear uptil and including endYear
   private getYearRange: (startYear: number, endYear: number) => number[] = (
     startYear,
     endYear
   ) => {
-    let years = [];
+    let numberOfYears: number = endYear - startYear;
+    let years: number[] = [];
 
-    for (let i = 0; i <= endYear; i++) {
+    for (let i = 0; i <= numberOfYears; i++) {
       const focusedYear = startYear + i;
       years.push(focusedYear);
     }
@@ -30,17 +50,39 @@ class CalendarComponent extends Component<PropRules, StateRules> {
     return years;
   };
 
-  private getMonthRange: () => number[] = () => {
-    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  private getMonthRange: () => MonthInfo[] = () => {
+    const monthLabels = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const monthNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    let months: MonthInfo[] = [];
+
+    for (let i = 0; i < 12; i++) {
+      months.push({
+        label: monthLabels[i],
+        number: monthNumbers[i],
+      });
+    }
+
+    return months;
   };
 
   private getDateRange: (month: number, year: number) => DateRangeInfo = (
     month,
     year
   ) => {
-    // Since the new Date() constructor's month parameter interprets month order by 0-11 instead of 1-12, we'll
-    // have to ensure this function's parameter goes by the same rule.
-    month = month - 1;
     const nextMonth: number = month + 1 > 12 ? 1 : month + 1;
     const monthYear: number = nextMonth === 1 ? year + 1 : year;
 
@@ -56,6 +98,10 @@ class CalendarComponent extends Component<PropRules, StateRules> {
       days.push(i + 1);
     }
 */
+    console.log({
+      days: numberOfDays,
+      padding: weekdayPadding,
+    });
     return {
       days: numberOfDays,
       padding: weekdayPadding,
@@ -66,6 +112,13 @@ class CalendarComponent extends Component<PropRules, StateRules> {
     padding
   ) => {
     const blocks: JSX.Element[] = [];
+
+    // The week starts on Sunday. If there is no weekday padding based on the date, then add a full
+    // padding to ensure the first months date starts on sunday.
+
+    if (padding === 0) {
+      padding = 7;
+    }
 
     for (let i = 0; i < padding; i++) {
       blocks.push(<td key={"padding-id-" + i}></td>);
@@ -98,8 +151,6 @@ class CalendarComponent extends Component<PropRules, StateRules> {
     const rows: JSX.Element[] = [];
     let row: JSX.Element[] = [];
 
-    console.log(blocks);
-
     for (let i = 0; i <= blocks.length; i++) {
       row.push(blocks[i]);
 
@@ -111,29 +162,134 @@ class CalendarComponent extends Component<PropRules, StateRules> {
       if (i > blocks.length - 1)
         rows.push(<tr key={"month-row-id-last"}>{row}</tr>);
     }
-    console.log(rows[0]);
 
     return <tbody>{rows}</tbody>;
   };
 
+  private renderTableHeader: () => JSX.Element = () => {
+    const weekdays: string[] = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+    const weekdaysBlocks: () => JSX.Element[] = () => {
+      return weekdays.map((weekday, index) => (
+        <th key={"calendar-header-block-" + index}>{weekday}</th>
+      ));
+    };
+
+    return (
+      <thead>
+        <tr>{weekdaysBlocks()}</tr>
+      </thead>
+    );
+  };
+
+  private renderYearSelector: (
+    startYear: number,
+    endYear: number,
+    handleChange: (input: React.ChangeEvent<HTMLSelectElement>) => void
+  ) => JSX.Element = (startYear, endYear, handleChange) => {
+    const years: number[] = this.getYearRange(startYear, endYear);
+    const { selectedYear } = this.state;
+
+    const optionElements: JSX.Element[] = years.map((year, index) => (
+      <option
+        value={year}
+        selected={year === selectedYear}
+        key={"calendar-selectable-year-id-" + index}
+      >
+        {year}
+      </option>
+    ));
+
+    return (
+      <select title="Select year" onChange={handleChange}>
+        {optionElements}
+      </select>
+    );
+  };
+
+  private renderMonthSelector: (
+    handleChange: (input: React.ChangeEvent<HTMLSelectElement>) => void
+  ) => JSX.Element = (handleChange) => {
+    const months: MonthInfo[] = this.getMonthRange();
+    const { selectedMonth } = this.state;
+    console.log("MONTH", selectedMonth);
+    const optionElements: JSX.Element[] = months.map((month, index) => (
+      <option
+        value={month.number}
+        selected={month.number === (selectedMonth % 12) + 1}
+        key={"calendar-selectable-month-id-" + index}
+      >
+        {month.label}
+      </option>
+    ));
+
+    return (
+      <select title="Select month" onChange={handleChange}>
+        {optionElements}
+      </select>
+    );
+  };
+
+  private handleMonthSelectorChange: (
+    input: React.ChangeEvent<HTMLSelectElement>
+  ) => void = (input) => {
+    const { value } = input.currentTarget;
+
+    //this.setState({ selectedMonth: parseInt(value) });
+    this.setMonthState(parseInt(value) - 1);
+  };
+
+  private handleYearSelectorChange: (
+    input: React.ChangeEvent<HTMLSelectElement>
+  ) => void = (input) => {
+    const { value } = input.currentTarget;
+
+    //this.setState({ selectedYear: parseInt(value) });
+    this.setYearState(parseInt(value));
+  };
+
+  setMonthState: (value: number) => void = (value) => {
+    let adjustedValue: number = value;
+    const { selectedYear } = this.state;
+
+    if (value < 0) {
+      adjustedValue = 11;
+      this.setYearState(selectedYear - 1);
+    }
+    if (value > 11) {
+      adjustedValue = 0;
+      this.setYearState(selectedYear + 1);
+    }
+
+    this.setState({ selectedMonth: adjustedValue });
+  };
+
+  setYearState: (value: number) => void = (value) => {
+    this.setState({ selectedYear: value });
+  };
+
   render = () => {
     const { id, refer } = this.props;
+    const { selectedMonth, selectedYear } = this.state;
 
     return (
       <div id={id} ref={refer} className="calendar-container">
+        <div className="calendar-navigation-container">
+          <IconComponent
+            iconReference="fas fa-angle-left"
+            className="calendar-nav-arrow"
+            onClick={() => this.setMonthState(selectedMonth - 1)}
+          />
+          {this.renderMonthSelector(this.handleMonthSelectorChange)}
+          {this.renderYearSelector(2010, 2030, this.handleYearSelectorChange)}
+          <IconComponent
+            iconReference="fas fa-angle-right"
+            className="calendar-nav-arrow"
+            onClick={() => this.setMonthState(selectedMonth + 1)}
+          />
+        </div>
         <table className="calendar-table">
-          <thead>
-            <tr>
-              <th>Mo</th>
-              <th>Tu</th>
-              <th>We</th>
-              <th>Th</th>
-              <th>Fr</th>
-              <th>Sa</th>
-              <th>Su</th>
-            </tr>
-          </thead>
-          {this.renderDays(2, 2020)}
+          {this.renderTableHeader()}
+          {this.renderDays(selectedMonth, selectedYear)}
         </table>
       </div>
     );
